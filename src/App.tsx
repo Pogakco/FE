e8cab5dd7234b7bc126f66bef22e8a88bad96a86
하주영 <ju980304@naver.com>
2024-06-29 01:35:08 +0900

    Feat: gloal Style 적용 및 라우터 초기 설정
    - light Theme 적용
    - Pretendard font
    - router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { light } from "./style/theme";
+import GlobalStyle from "./style/global";
+import UserSignup from "./pages/UserSignup";
+import UserLogin from "./pages/UserLogin";
+import "@/style/font.css";
+
+const routeList = [
+  { path: "/signup", element: <UserSignup /> },
+  { path: "/login", element: <UserLogin /> }
+];
+
+const router = createBrowserRouter(
+  routeList.map((route) => {
+    return {
+      ...route,
+      errorElement: <main>error 페이지</main>
+    };
+  })
+);
 
 function App() {
-  return <div></div>;
+  return (
+    <ThemeProvider theme={light}>
+      <GlobalStyle themeName="light" />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/UserSignup.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const UserSignup = () => {
+  return (
+    <UserSignupStyle>
+      <h1>UserSignup body</h1>
+    </UserSignupStyle>
+  );
+};
+
+const UserSignupStyle = styled.main``;
+export default UserSignup;
