e8122e5f014573cd2ec8268944a38cf2395fd71c
changchangwoo <changchangwoo@naver.com>
2024-06-30 00:35:14 +0900

    Remove : Temp파일 삭제

--- src/pages/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const URL =
-  import.meta.env.MODE === "production" ? "/" : "http://localhost:5173";
-
-export const socket = io(URL, {
-  withCredentials: true, // 쿠키를 서버로 전달하기 위한 설정
-});
