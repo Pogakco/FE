6f4bb7d44ff6a749ad96b0523cd6aecb8983bf39
changchangwoo <changchangwoo@naver.com>
2024-07-06 00:35:39 +0900

    Feat : mock 환경설정 및 예제코드 추가

--- src/api/roomList.api.ts
+++ src/api/roomList.api.ts
@@ -3,5 +3,6 @@ import { requestHandler } from "./apiClient";
 
 export const fetchRooms = async (page : string = "1", isRunningChecked : boolean = false)  => {
     let num = Number(page);
-    return await requestHandler<IroomListData>("get", `/rooms?page=${num}&is_running=${isRunningChecked}`);
+    // return await requestHandler<IroomListData>("get", `/rooms?page=${num}&is_running=${isRunningChecked}`);
+    return await requestHandler<IroomListData>("get", '/Mockrooms');
 }
