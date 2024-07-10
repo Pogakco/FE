62b0d3f25f14b2e3db764367656b201285bcb877
changchangwoo <changchangwoo@naver.com>
2024-07-10 17:41:08 +0900

    Feat : isJoined 유무에 따른 API 호출 분리

--- src/hooks/mutations/useJoinRoom.ts
+++ src/hooks/mutations/useJoinRoom.ts
@@ -1,13 +1,15 @@
 import { joinRoom } from "@/api/roomList.api";
 import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 const useJoinRoom = (
   roomId: number
 ): UseMutationResult<any, Error, void, unknown> => {
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: () => joinRoom(roomId),
-    onSuccess: (data) => {
-      if (data.status === 204) console.log("성공");
+    onSuccess: () => {
+      navigate(`/rooms/${roomId}`)
     }
 })
 };
