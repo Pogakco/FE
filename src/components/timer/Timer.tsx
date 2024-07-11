d72943b50b428b122bc63afca9f596f5122f4fe5
하주영 <ju980304@naver.com>
2024-07-11 16:08:05 +0900

    Feat: react-circular-progressbar 설치

--- src/components/timer/Timer.tsx
+++ src/components/timer/Timer.tsx
@@ -1,18 +1,30 @@
 import { TtimerStatus } from "@/models/timer.model";
 import { formatTime } from "@/utils/formatTime";
-import styled from "styled-components";
+import { useEffect } from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import styled, { useTheme } from "styled-components";
 
 interface Props {
   timerTime: number;
   status: TtimerStatus;
 }
 
-const Timer = ({ timerTime, status }: Props) => {
-  return (
-    <TimerContainer $isCritical={timerTime <= 3} $status={status}>
-      {formatTime(timerTime)}
-    </TimerContainer>
-  );
+const percent = (status: TtimerStatus, current: number) => {
+  let total;
+  switch (status) {
+    case "focusTime":
+      total = 25;
+      break;
+    case "shortBreakTime":
+      total = 5;
+      break;
+    case "longBreakTime":
+      total = 30;
+      break;
+    default:
+      return 0;
+  }
+  return ((total - current + 1) / total) * 100;
 };
 
 const getColorByStatus = (status: TtimerStatus, theme: any) => {
@@ -28,7 +40,61 @@ const getColorByStatus = (status: TtimerStatus, theme: any) => {
   }
 };
 
-const TimerContainer = styled.div<{ $isCritical: boolean; $status: TtimerStatus }>`
+const Timer = ({ timerTime, status }: Props) => {
+  const theme = useTheme();
+  const progressBarColor = getColorByStatus(status, theme);
+  useEffect(() => {
+    if (timerTime === 0) {
+      // 타이머 컴포넌트 제렌더링하여 100프로였떤 집중시간 -> 0프로로 되돌아가는 작업없이 바로 쉬는시간 0부터 시작하게끔
+    }
+  }, [timerTime]);
+
+  return (
+    <TimerStyle>
+      <svg style={{ height: 0, width: 0 }}>
+        <defs>
+          <linearGradient id="progress" gradientTransform="rotate(90)">
+            <stop offset="0%" stopColor="#FF0068" />
+            <stop offset="70%" stopColor="#FF4D27" />
+            <stop offset="100%" stopColor="#FF7A01" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgressbar
+        className="circular"
+        value={percent(status, timerTime)}
+        strokeWidth={8}
+        styles={{
+          path: {
+            // stroke: `url(#progress)`,
+            stroke: progressBarColor,
+            height: "100%",
+            strokeLinecap: "round",
+            transition: "stroke-dashoffset 1s linear 0s"
+          },
+          trail: {
+            stroke: "white"
+          }
+        }}
+      />
+      <TimerContainer $isCritical={timerTime <= 3} $status={status}>
+        {formatTime(timerTime)}
+      </TimerContainer>
+    </TimerStyle>
+  );
+};
+
+const TimerStyle = styled.div`
+  .circular {
+    width: 335px;
+    height: 335px;
+  }
+`;
+
+const TimerContainer = styled.div<{
+  $isCritical: boolean;
+  $status: TtimerStatus;
+}>`
   width: 335px;
   height: 335px;
   border-radius: 335px;
--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -6,55 +6,67 @@ import { getTimerTime } from "@/utils/getTimerTime";
 import { useEffect, useState } from "react";
 
 interface IuseTimer {
-    roomData: IroomData | undefined;
-    syncedIsRunning : boolean | null;
-    syncedStartedAt : string | null;
-    syncedCurrentCycles : number | null;
+  roomData: IroomData | undefined;
+  syncedIsRunning: boolean | null;
+  syncedStartedAt: string | null;
+  syncedCurrentCycles: number | null;
 }
 
-const useTimer = ({roomData, syncedStartedAt, syncedIsRunning, syncedCurrentCycles} : IuseTimer) 
-: {timerTime : number, status : TtimerStatus} => {
-    const [timerTime, setTimerTime] = useState<number>(0);
-    const [status, setStatus] = useState<TtimerStatus>(SOCKET_TIMER_STATUS.SHORT_BREAK_TIME);
+const useTimer = ({
+  roomData,
+  syncedStartedAt,
+  syncedIsRunning,
+  syncedCurrentCycles
+}: IuseTimer): { timerTime: number; status: TtimerStatus } => {
+  const [timerTime, setTimerTime] = useState<number>(0);
+  const [status, setStatus] = useState<TtimerStatus>(
+    SOCKET_TIMER_STATUS.SHORT_BREAK_TIME
+  );
 
-    useEffect(() => {
-        if (!roomData) return;
-        const startAt = syncedStartedAt ? syncedStartedAt : roomData.startedAt;
-        const isRunning = syncedIsRunning ? syncedIsRunning : roomData.isRunning;
-        if (!isRunning) {
+  useEffect(() => {
+    if (!roomData) return;
+    const startAt = syncedStartedAt ? syncedStartedAt : roomData.startedAt;
+    const isRunning = syncedIsRunning ? syncedIsRunning : roomData.isRunning;
+    if (!isRunning) {
+      setTimerTime(roomData.focusTime);
+      setStatus(SOCKET_TIMER_STATUS.SHORT_BREAK_TIME);
+    }
+
+    if (isRunning) {
+      const interval = setInterval(() => {
+        const differenceTime = getDifferentTime(startAt);
+        console.log("차이시간", differenceTime);
+        const { focusTime, shortBreakTime, totalCycles, longBreakTime } =
+          roomData;
+        const { status, timerData } = getTimerTime(
+          differenceTime,
+          focusTime,
+          shortBreakTime,
+          totalCycles,
+          longBreakTime
+        );
+        setStatus(status);
+
+        if (status === SOCKET_TIMER_STATUS.SET) {
+          console.log(
+            syncedStartedAt,
+            syncedCurrentCycles,
+            syncedIsRunning,
+            roomData
+          );
           setTimerTime(roomData.focusTime);
-          setStatus(SOCKET_TIMER_STATUS.SHORT_BREAK_TIME)
+          clearInterval(interval);
+          setStatus(SOCKET_TIMER_STATUS.SHORT_BREAK_TIME);
+        } else if (status) {
+          setTimerTime(timerData);
         }
-    
-        if (isRunning) {
-          const interval = setInterval(() => {
-            const differenceTime = getDifferentTime(startAt);
-            console.log('차이시간', differenceTime)
-            const { focusTime, shortBreakTime, totalCycles, longBreakTime } = roomData;
-            const { status, timerData } = getTimerTime(
-              differenceTime,
-              focusTime,
-              shortBreakTime,
-              totalCycles,
-              longBreakTime
-            );
-            setStatus(status);
-    
-            if (status === SOCKET_TIMER_STATUS.SET) {
-              console.log(syncedStartedAt, syncedCurrentCycles, syncedIsRunning, roomData)
-              setTimerTime(roomData.focusTime);
-              clearInterval(interval);
-              setStatus(SOCKET_TIMER_STATUS.SHORT_BREAK_TIME)
-            } else if (status) {
-              setTimerTime(timerData);
-            }
-          }, 1000);
-    
-          return () => clearInterval(interval);
-        }
-      }, [syncedStartedAt, syncedCurrentCycles, syncedIsRunning, roomData]);
+      }, 1000);
 
-      return { timerTime, status};
-}
+      return () => clearInterval(interval);
+    }
+  }, [syncedStartedAt, syncedCurrentCycles, syncedIsRunning, roomData]);
 
-export default useTimer
+  return { timerTime, status };
+};
+
+export default useTimer;
