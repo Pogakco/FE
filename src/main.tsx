b9908ecae747d99b10b67f34b0295cce91d706e3
하주영 <ju980304@naver.com>
2024-07-13 15:51:54 +0900

    Chore: devtools 설치

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./api/queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 // if (process.env.NODE_ENV === "development") {
 //   import("./mocks/browser.ts").then(({ worker }) => {
@@ -12,5 +13,6 @@ import { queryClient } from "./api/queryClient.ts";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
