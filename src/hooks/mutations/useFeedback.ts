9ceaca0ed4e318c8708c743c62800b60c3b4412e
하주영 <ju980304@naver.com>
2024-07-16 12:19:08 +0900

    Fix: 피드백 api에서 동일한 데이터로 재 요청시 요청 방지

--- src/hooks/mutations/useFeedback.ts
+++ src/hooks/mutations/useFeedback.ts
@@ -1,14 +1,31 @@
 import { feedback } from "@/api/feedback.api";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 
 const useFeedback = () => {
+  const [lastRequestData, setLastRequestData] = useState<{
+    contents: string;
+  } | null>(null);
+
   const {
     mutate: feedbackMutate,
     isSuccess,
     isPending,
     reset
   } = useMutation({
-    mutationFn: (reqBody: { contents: string }) => feedback(reqBody),
+    mutationFn: async (reqBody: { contents: string }) => {
+      // 이전 요청과 동일하다면 요청 방지
+      if (JSON.stringify(reqBody) === JSON.stringify(lastRequestData)) {
+        throw new Error("이전 요청과 동일한 데이터입니다. 요청을 방지합니다.");
+      }
+
+      const response = await feedback(reqBody);
+
+      setLastRequestData(reqBody);
+
+      return response;
+    },
+
     onError: () => {
       reset();
     }
